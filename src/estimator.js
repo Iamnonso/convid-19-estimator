f096027b0c763dd8f498d885c36d746c2d74e6fe
Nwaduba Emmanuel Chinonso <iamnonso@volttech.com.ng>
2020-04-07 22:52:35 -0700

    first commit

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,45 @@
-const covid19ImpactEstimator = (data) => data;
+const periodInDays = (months) => months * 30;
+
+const periodWeeks = (weeks) => weeks * 7;
+
+const NumberOfDays = (periodType, timeToElapse) => {
+  switch (periodType) {
+    case 'months':
+      return periodInDays(timeToElapse);
+    case 'weeks':
+      return periodWeeks(timeToElapse);
+    default:
+      return timeToElapse;
+  }
+};
+
+const covid19ImpactEstimator = (data) => {
+
+  const {
+    reportedCases,
+    periodType,
+    timeToElapse,
+  } = data;
+
+  const impact = {};
+
+  const severeImpact = {};
+
+  impact.currentlyInfected = reportedCases * 10;
+
+  severeImpact.currentlyInfected = reportedCases * 50;
+
+  const projected = Math.trunc(NumberOfDays(periodType, timeToElapse) / 3);
+
+  impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** projected);
+
+  severeImpact.infectionsByRequestedTime = severeImpact.currentlyInfected * (2 ** projected);
+
+  return {
+    data,
+    impact,
+    severeImpact
+  };
+};
 
 export default covid19ImpactEstimator;
