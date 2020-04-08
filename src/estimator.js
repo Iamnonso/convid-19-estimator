5041989d094e2abbe19416efb747a638dad8057b
Nwaduba Emmanuel Chinonso <iamnonso@volttech.com.ng>
2020-04-07 23:31:03 -0700

    resolve issue

--- src/estimator.js
+++ src/estimator.js
@@ -1,5 +1,4 @@
 const periodInDays = (months) => months * 30;
-
 const periodWeeks = (weeks) => weeks * 7;
 
 const NumberOfDays = (periodType, timeToElapse) => {
@@ -14,22 +13,19 @@ const NumberOfDays = (periodType, timeToElapse) => {
 };
 
 const covid19ImpactEstimator = (data) => {
-
   const {
     reportedCases,
     periodType,
-    timeToElapse,
+    timeToElapse
   } = data;
 
   const impact = {};
-
   const severeImpact = {};
 
   impact.currentlyInfected = reportedCases * 10;
-
   severeImpact.currentlyInfected = reportedCases * 50;
 
-  const projected = Math.trunc(NumberOfDays(periodType, timeToElapse) / 3);
+  const projected = Math.round(NumberOfDays(periodType, timeToElapse) / 3);
 
   impact.infectionsByRequestedTime = impact.currentlyInfected * (2 ** projected);
 
